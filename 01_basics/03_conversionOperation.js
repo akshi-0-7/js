c76c4ef7ef6e5e9315c23bca481672607726ef8a
akshi-0-7 <143407102+akshi-0-7@users.noreply.github.com>
2025-09-02 08:23:56 +0000

    datatype conv and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,40 @@ let id = true
 let strconv = String(id)
 console.log(strconv)
 console.log(typeof strconv)
+
+// ************OPERATIONS************
+
+let value = 3
+let negValue = -value //simply negating the value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2); ******basic arithmatic operations
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "are you okay bro"
+let str2 = " my handsome bro"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);         left to right assign hogi data type, agr phle string h then string else number depending on the situation
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);  increment operation
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 // never do this, dec readability
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
