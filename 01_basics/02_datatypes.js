81352cd161332c7fe4150d862f05e78b840689bd
akshi-0-7 <143407102+akshi-0-7@users.noreply.github.com>
2025-09-02 17:00:16 +0000

    datatype and memory type

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,8 +3,8 @@
 //alert("hello") //wont work as this works only on browser not nodejs
 //alert(3+3);  // wont work
 
-console.log(3+3) //console.log(2+2); console.log("akshi") we can write like this too
-console.log("akshi") // but it will affect readibility so we avoid it
+// console.log(3+3) //console.log(2+2); console.log("akshi") we can write like this too
+// console.log("akshi") // but it will affect readibility so we avoid it
 
 let name = "Akshi" //string
 let age = 22 //number
@@ -24,13 +24,13 @@ symbol -> unique
 
 // object
 
-console.log(typeof"akshi")
-console.log(typeof undefined) // type is undefined
-console.log(typeof null) // type is object
+// console.log(typeof"akshi")
+// console.log(typeof undefined) // type is undefined
+// console.log(typeof null) // type is object
 
 
 //***** types of data on the level of abstraction******
-// premitive -> string, bool, number, null, undefined, symbol, bigint
+// primitive(stack) -> string, bool, number, null, undefined, symbol, bigint
 const score = 100 //number
 const scoreValue = 100.3 //number
 
@@ -43,7 +43,7 @@ const anotherId = Symbol('123') //tho symbol of both are same but they are not e
 
 console.log(id === anotherId);//return false
 
-// non premitive/ reference -> array, objects, functions
+// non premitive/ reference(heap) -> array, objects, functions
 const gmmtv = ["Pond", "Phuwin", "Joong", "Dunk"]; // array
 let myObj = {
     name: "Polcasan",      //obj
@@ -54,4 +54,27 @@ const myFunction = function(){            //func
     console.log("Hello world");
 }
 
-console.log(gmmtv);
+// console.log(gmmtv);
+
+//types of memory-> stack(primitive), heap(non-primitive)
+
+let yt="gmmtvofficial.com"
+let yt2= yt
+yt2="wetvofficial.com"  //stack me uski copy dete hain
+/*
+yt2(wetv)
+yt2(yt)
+yt(gmm)
+to stack me jo latest yt2 ki value hogi vo return ho jayegi ie. wetv as a copy
+*/ 
+console.log(yt)
+console.log(yt2)
+
+let user1={
+    name: "Pond",
+    faen: "Phuwin"
+}
+let user2= user1;
+user2.faen= "phuphu" //idhr ye stack me nahi h so copy nahi dega, instead ye reference dega ki user2 me jo update kra hai faen ka vo reference ki vjh se 1 me bhi hoga 
+console.log(user1);
+console.log(user2);
