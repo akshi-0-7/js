88675b4201f19d83c708c7b82895ba70d1486577
akshi-0-7 <143407102+akshi-0-7@users.noreply.github.com>
2025-09-02 08:07:34 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score="akshi"
+
+//console.log(typeof score);
+//console.log(typeof (score))
+
+let numconversion= Number(score)
+//console.log(typeof numconversion);
+//console.log(numconversion);
+
+// "33"-> 33
+// "akshi" -> NaN
+// true -> 1 false ->0
+
+let IsLoggedIn=""
+
+let bolconversion= Boolean(IsLoggedIn)
+//console.log(bolconversion)
+
+//jb hum bool me convert krte hai and pass any value then true, 0 and empty then false
+
+let id = true
+let strconv = String(id)
+console.log(strconv)
+console.log(typeof strconv)
