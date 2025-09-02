903567fb2c9bebd74ef1999d79633ef3f12c3428
akshi-0-7 <143407102+akshi-0-7@users.noreply.github.com>
2025-09-02 08:06:26 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello") //wont work as this works only on browser not nodejs
+//alert(3+3);  // wont work
+
+console.log(3+3) //console.log(2+2); console.log("akshi") we can write like this too
+console.log("akshi") // but it will affect readibility so we avoid it
+
+let name = "Akshi" //string
+let age = 22 //number
+let IsLoggedIn= true //boolean
+let state; //undefined
+
+// data types
+/* number -> 2^52/53 ki range tk
+bigint -> number ki range se jada mostly jaha large number use hote h like in trade etc
+string -> ""
+boolean -> true/false
+null -> standalone value, ye empty show krta hai ki value hai he ni
+undefined -> jb hum define he na kare, koi value ho skti h unlike null
+symbol -> unique
+*/
+
+
+// object
+
+console.log(typeof"akshi")
+console.log(typeof undefined) // type is undefined
+console.log(typeof null) // type is object
